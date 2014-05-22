7dc427d3c119376c52231cb0ceb76acd19246334
CgminerMonitor <cgminermonitor.com@gmail.com>
2014-05-22 21:21:21 +0200

    controlling worker from web part 8 - implement cgminer config writing and reading

--- CgminerMonitorClient/Workers/Control/CgminerConfigCommandHandler.cs
+++ CgminerMonitorClient/Workers/Control/CgminerConfigCommandHandler.cs
@@ -1,5 +1,7 @@
-﻿using CgminerMonitorClient.CgminerMonitor.Common;
+﻿using System.IO;
+using CgminerMonitorClient.CgminerMonitor.Common;
 using CgminerMonitorClient.Configuration;
+using CgminerMonitorClient.Utils;
 
 namespace CgminerMonitorClient.Workers.Control
 {
@@ -14,12 +16,16 @@ namespace CgminerMonitorClient.Workers.Control
 
         public WorkerCommandResponse Read(WorkerCommand command)
         {
-            throw new System.NotImplementedException();
+            Log.Instance.InfoFormat("Executing read cgminer config command.");
+            var configFile = File.ReadAllText(_controlConfig.CgminerConfigFileLocation);
+            return WorkerCommandResponse.Success(command.Id, configFile);
         }
 
         public WorkerCommandResponse Write(WorkerCommand command)
         {
-            throw new System.NotImplementedException();
+            Log.Instance.InfoFormat("Executing write cgminer config command.");
+            File.WriteAllText(_controlConfig.CgminerConfigFileLocation, command.Value);
+            return WorkerCommandResponse.Success(command.Id, null);
         }
     }
 }
